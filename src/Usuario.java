9f20f54521a801014a1af1dfd61653e1c557e4b9
Gabriel_Morales <toni.morales.gmg@gmail.com>
2023-08-30 11:45:35 -0600

    30/08/2023 v1

---
+++ src/Usuario.java
@@ -0,0 +1,45 @@
+public class Usuario {
+    //Atributos
+    private int id;
+    private String nombre;
+    private String correo;
+    private String matricula;
+    private String telefono;
+
+    public Usuario(String nombre, String correo){
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
